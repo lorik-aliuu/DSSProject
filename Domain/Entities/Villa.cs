a436fafe2c7ab8803fac6256747e75e5d734a275
Lorik Aliu <loriku123321@gmail.com>
2024-12-01 19:28:06 +0100

    added Villa model

---
+++ Domain/Entities/Villa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Villa
+    {
+
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int SquareFoot { get; set; }
+
+        public int Occupancy { get; set; }
+
+        public string? imageUrl { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
