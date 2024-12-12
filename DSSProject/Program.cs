11933a1444fc55c6589bf70aa669e5461b4d4606
Lorik Aliu <loriku123321@gmail.com>
2024-12-12 16:38:10 +0100

    fixed errors

--- DSSProject/Program.cs
+++ DSSProject/Program.cs
@@ -1,7 +1,13 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("LorikConnection")));
 
 var app = builder.Build();
 
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Premium Pool Villa",
+                    Description = "A lavish villa with a private pool, lush views, and unmatched elegance",
+                    imageUrl = "",
+                    Occupancy = 4,
+                    SquareFoot = 470,
+                },
+                new Villa
+                {
+                    Id = 2,
+                    Name = "Azure Heaven",
+                    Description = "A tranquil escape with endless blue skies and a shimmering private pool",
+                    imageUrl = "",
+                    Occupancy = 6,
+                    SquareFoot = 520,
+                }
+            );
+        }
+    }
+}
